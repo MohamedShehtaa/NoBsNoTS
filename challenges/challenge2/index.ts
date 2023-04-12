7f0b15b187de2653aa0d410074e91c1b0a555979
MohamedShehtaa <mmshehta92@gmail.com>
2023-04-12 17:26:54 +0200

    add implementation for filter method

--- challenges/challenge2/index.ts
+++ challenges/challenge2/index.ts
@@ -8,3 +8,10 @@ function myForEach <T>(items:T[], cb:(v:T)=> void ) :void {
 
 myForEach([1,2,3],(v) => console.log(v))
 
+// filter method
+function myFilter<T>(items:T[],cb:(V:T) => boolean): T[]{
+    return items.reduce((a:T[],v) => ( cb(v) ? [...a,v] : a) ,[])
+}
+
+console.log(myFilter([1,2,3,4,5,6,7,8],(v)=> v%2 ===0 ))
+
